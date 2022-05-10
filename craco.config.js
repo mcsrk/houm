c3961d9ff5a3b0dd9c09692150c8bee0633e5168
mcsrk <49255542+mcsrk@users.noreply.github.com>
2022-05-10 16:32:43 -0500

    :plus: added craco-less to overrite antd color palette

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
+const CracoLessPlugin = require("craco-less");
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#FF452B",
+              "@layout-header-background": "#FFFFFF",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
