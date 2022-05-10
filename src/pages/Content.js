f41aa7cd45ec346093899695d03c53e63a8c806b
mcsrk <49255542+mcsrk@users.noreply.github.com>
2022-05-09 21:47:19 -0500

    🎨 improved content code

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Layout } from "antd";
 
 import { getRequest } from "axiosClient";
 import SearchHero from "components/content/search/SearchHero";
 import SongsList from "components/content/SongsList";
 import { useDebouncedEffect } from "utils/utils";
+import ArtistsList from "components/content/ArtistsList";
 
 const { Content } = Layout;
 
 const ContentPage = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); 
 
   const [searchTerm, setSearchTerm] = useState("tame impala");
   const [queryType, setQueryType] = useState("tracks");
@@ -17,30 +18,39 @@ const ContentPage = () => {
   const [limit, setLimit] = useState("10");
   const [numOfTopResult, setNumOfTopResult] = useState("5");
 
-  const [tracks, setTracks] = useState();
+  const [tracks, setTracks] = useState([]);
+  const [artists, setArtist] = useState([]);
+  const [albums, setAlbums] = useState([]);
 
+  const isArtist = queryType === "artists";
+  const isTrack = queryType === "tracks";
+  const isAlbum = queryType === "albums";
+
+  //Generic fetching function for different query types
   const fetchSearchData = async () => {
     setLoading(true);
+    const config = {
+      params: {
+        q: searchTerm,
+        type: queryType,
+        pag_offset: offset,
+        pag_limit: limit,
+        numberOfTopResults: numOfTopResult,
+      },
+    };
+    const setters = {
+      tracks: setTracks,
+      artists: setArtist,
+      albums: setAlbums,
+    };
     try {
-      const config = {
-        params: {
-          q: searchTerm,
-          type: queryType,
-          pag_offset: offset,
-          pag_limit: limit,
-          numberOfTopResults: numOfTopResult,
-        },
-      };
       const searchRes = await getRequest("search", config);
-      console.log(searchRes.data.tracks.items);
-      setTracks(searchRes.data.tracks.items);
-      setLoading(false);
+      setters[queryType](searchRes.data[queryType].items);
     } catch (error) {
-      //Log errors
       console.log(error);
-      setTracks([]);
-      setLoading(false);
+      setters[queryType]([]);
     }
+    setLoading(false);
   };
 
   useDebouncedEffect(
@@ -55,10 +65,13 @@ const ContentPage = () => {
         setSearchTerm={setSearchTerm}
         searchTerm={searchTerm}
         onSearch={fetchSearchData}
+        queryType={queryType}
+        setQueryType={setQueryType}
       />
-
       <div className="container mx-44 my-8 w-9/12 text-center">
-        <SongsList loading={loading} tracks={tracks} />
+        {isTrack && <SongsList loading={loading} tracks={tracks} />}
+        {isArtist && <ArtistsList loading={loading} artists={artists} />}
+        {isAlbum && <span></span>}
       </div>
     </Content>
   );
